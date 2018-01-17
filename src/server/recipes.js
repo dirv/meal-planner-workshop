c423a0094fbb59272916b573751567cf68352c2a
Daniel Irvine <daniel@danielirvine.com>
2018-01-17 19:06:26 +0000

    Add validation of recipes

--- src/server/recipes.js
+++ src/server/recipes.js
@@ -7,9 +7,32 @@ export const recipes = [{
   }],
   instructions: 'Here are the instructions for the recipe.',
   serves: 1,
-  prepartionTimeInMins: 3
+  preparationTimeInMins: 3
 }]
 
 export function addRecipe(recipe) {
   recipes.push(recipe)
+}
+
+export const recipeSchema = {
+  type: 'object',
+  properties: {
+    name: { type: 'string' },
+    instructions: { type: 'string' },
+    serves: { type: 'number' },
+    preparationTimeInMins: { type: 'number' },
+    ingredients: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          name: { type: 'string' },
+          amount: { type: 'number' },
+          unit: { type: 'string' }
+        },
+        required: ['name', 'amount', 'unit']
+      }
+    },
+  },
+  required: ['name', 'instructions', 'serves', 'preparationTimeInMins', 'ingredients']
 }
--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,10 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const app = express()
+import { Validator, ValidationError } from 'express-json-validator-middleware'
 import { ingredients } from './ingredients'
-import { recipes, addRecipe } from './recipes'
+import { recipes, addRecipe, recipeSchema } from './recipes'
+
+var validator = new Validator({allErrors: true})
 
 app.get('/', function (req, res) {
   res.send(ingredients);
@@ -11,7 +14,10 @@ app.get('/recipes', function(req, res) {
   res.send(recipes.map(r => r.name))
 })
 
-app.post('/recipes', express.json(), function(req, res) {
+app.post('/recipes', [
+  express.json(),
+  validator.validate({body: recipeSchema})
+], function(req, res) {
   addRecipe(req.body)
   res.sendStatus(200)
 })
@@ -27,5 +33,16 @@ app.get('/recipes/:name', function(req, res) {
 
 app.set('json spaces', 2);
 app.use(express.json())
- 
+
+app.use(function(err, req, res, next) {
+  let responseData;
+
+  if (err.name === 'JsonSchemaValidationError') {
+    res.status(400)
+    res.send(err.validationErrors)
+  } else {
+    next(err);
+  }
+});
+
 app.listen(3000)
